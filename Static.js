7b4a09f9addfca05d21c94165b65844ae7fc1157
Kevin Rico <krico315@gmail.com>
2022-10-19 22:59:26 -0600

    WIP COMMIT

---
+++ Static.js
@@ -0,0 +1,9 @@
+class Governor{
+    static publisher = "Anna Romero";
+    static description = "A Governor needs to be confident in stressful situation."
+    static staticProperty = 'An basic understanding of the general public and their needs.'
+}
+
+alert(Governor.publisher); 
+
+
